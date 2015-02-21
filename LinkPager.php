3f42ba3b218f7112fe6dd03f88ee002e37d0d9f6
Justin Voelker <justin@justinvoelker.com>
2015-02-20 20:46:01 -0500

    Initial commit

---
+++ LinkPager.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace justinvoelker\separatedpager;
+
+use Yii;
+use yii\helpers\Html;
+
+/**
+ * LinkPager displays a list of hyperlinks that lead to different pages of target.
+ *
+ * LinkPager works with a [[Pagination]] object which specifies the totally number
+ * of pages and the current page number.
+ *
+ * Note that LinkPager only generates the necessary HTML markups. In order for it
+ * to look like a real pager, you should provide some CSS styles for it.
+ * With the default configuration, LinkPager should look good using Twitter Bootstrap CSS framework.
+ *
+ * separatedpager changes the default LinkPager behavior by always displaying the first and last
+ * pages separated from the current pages by an ellipsis (or any other string specified).
+ *
+ * @author Justin Voelker <justin@justinvoelker.com>
+ */
+class LinkPager extends \yii\widgets\LinkPager
+{
+    /**
+     * @var string the name of the input checkbox input fields. This will be appended with `[]` to ensure it is an array.
+     */
+    public $separator = '...';
+
+    /**
+     * Renders the page buttons.
+     * @return string the rendering result
+     */
+    protected function renderPageButtons()
+    {
+        $pageCount = $this->pagination->getPageCount();
+        if ($pageCount < 2 && $this->hideOnSinglePage) {
+            return '';
+        }
+
+        $buttons = [];
+        $currentPage = $this->pagination->getPage();
+
+        // first page
+        if ($this->firstPageLabel !== false) {
+            $buttons[] = $this->renderPageButton($this->firstPageLabel, 0, $this->firstPageCssClass, $currentPage <= 0,
+                false);
+        }
+
+        // prev page
+        if ($this->prevPageLabel !== false) {
+            if (($page = $currentPage - 1) < 0) {
+                $page = 0;
+            }
+            $buttons[] = $this->renderPageButton($this->prevPageLabel, $page, $this->prevPageCssClass,
+                $currentPage <= 0, false);
+        }
+
+        // page calculations
+        list($beginPage, $endPage) = $this->getPageRange();
+        $startSeparator = false;
+        $endSeparator = false;
+        $beginPage++;
+        $endPage--;
+        if ($beginPage != 1) {
+            $startSeparator = true;
+            $beginPage++;
+        }
+        if ($endPage + 1 != $pageCount - 1) {
+            $endSeparator = true;
+            $endPage--;
+        }
+
+        // smallest page
+        $buttons[] = $this->renderPageButton(1, 0, null, false, 0 == $currentPage);
+
+        // separator after smallest page
+        if ($startSeparator) {
+            $buttons[] = $this->renderPageButton($this->separator, null, null, true, false);
+        }
+        // internal pages
+        for ($i = $beginPage; $i <= $endPage; ++$i) {
+            if ($i != 0 && $i != $pageCount - 1) {
+                $buttons[] = $this->renderPageButton($i + 1, $i, null, false, $i == $currentPage);
+            }
+        }
+        // separator before largest page
+        if ($endSeparator) {
+            $buttons[] = $this->renderPageButton($this->separator, null, null, true, false);
+        }
+        // largest page
+        $buttons[] = $this->renderPageButton($pageCount, $pageCount - 1, null, false,
+            $pageCount - 1 == $currentPage);
+
+        // next page
+        if ($this->nextPageLabel !== false) {
+            if (($page = $currentPage + 1) >= $pageCount - 1) {
+                $page = $pageCount - 1;
+            }
+            $buttons[] = $this->renderPageButton($this->nextPageLabel, $page, $this->nextPageCssClass,
+                $currentPage >= $pageCount - 1, false);
+        }
+
+        // last page
+        if ($this->lastPageLabel !== false) {
+            $buttons[] = $this->renderPageButton($this->lastPageLabel, $pageCount - 1, $this->lastPageCssClass,
+                $currentPage >= $pageCount - 1, false);
+        }
+
+        return Html::tag('ul', implode("\n", $buttons), $this->options);
+    }
+}
---
+++ README.md
@@ -0,0 +1,60 @@
+#Separated Pager for Yii2
+
+A Yii2 LinkPager that displays the first and last pages inline with other pages.
+
+Rather than having dedicated "First" and "Last" buttons in your LinkPager, **Separated Pager** will show a standard set of page links but will also always include the first and last pages as the first and last page links. No more dedicated first/last buttons and no more guessing how many pages there are.
+
+##Installation
+
+###Install the extension
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist justinvoelker/yii2-separatedpager "*"
+```
+
+or add
+
+```
+"justinvoelker/yii2-separatedpager": "*"
+```
+
+to the require section of your `composer.json` file.
+
+##Usage
+
+Simply add the `pager` property to your GridView and reference this class
+
+```php
+GridView::widget([
+    'dataProvider' => $dataProvider,
+    ...
+    'pager' => [
+        'class' => 'justinvoelker\separatedpager\LinkPager',
+    ]
+]);
+```
+
+Please note that specifying less than 7 pages won't produce useful results.  Less than 7 pages of content is acceptable (will look like the standard LinkPager) but limiting the pager to something less than 7 pages will look and work poorly.  Five pages and the pager is almost worthless.  Less than 5 and it is worthless. 
+
+##Available Options
+
+In addition to all of the standard [LinkPager](www.yiiframework.com/doc-2.0/yii-widgets-linkpager.html) properties, one new property has been added, `separator`, that specifies a string to be used to indicate that multiple pages are being omitted.  The default separator is `...`.
+
+Here is my preferred pager setup.  A max of 7 pages, 'Previous' and 'Next' buttons spelled out that are hidden on extra-small screens.
+
+```php
+    'pager' => [
+        'class' => 'justinvoelker\separatedpager\CustomLinkPager',
+        'maxButtonCount' => 7,
+        'prevPageLabel' => 'Previous',
+        'nextPageLabel' => 'Next',
+        'prevPageCssClass' => 'prev hidden-xs',
+        'nextPageCssClass' => 'next hidden-xs',
+    ]
+```
+
+Keep in mind that setting css classes will overwrite the defaults rather than appending to them.  If additional classes should be included (such as the 'hidden-xs' above) the original `prev` and `next` should be included as well (this is the same way the standard LinkPager functions).
