0086a86da856d3baba4d92c2311408295952b57e
Igor Salevan <igor.salevan@gmail.com>
2015-05-15 12:38:41 +0300

    Update LinkPager.php
    Added "activeLinkable" property. It serves for turning off the anchor tag from the LinkPager active item.

--- LinkPager.php
+++ LinkPager.php
@@ -28,6 +28,12 @@ class LinkPager extends \yii\widgets\LinkPager
     public $separator = '...';
 
     /**
+     * @var boolean turns on|off the <a> tag for the active pager item. It is recommended to set it to false
+     * for the sake of Search Engines Optimisation. Defaults to true.
+     */
+    public $activeLinkable = true;
+
+    /**
      * Renders the page buttons.
      * @return string the rendering result
      */
@@ -108,5 +114,37 @@ class LinkPager extends \yii\widgets\LinkPager
         }
 
         return Html::tag('ul', implode("\n", $buttons), $this->options);
+    }
+
+    /**
+     * Renders a page button.
+     * You may override this method to customize the generation of page buttons.
+     * @param string $label the text label for the button
+     * @param integer $page the page number
+     * @param string $class the CSS class for the page button.
+     * @param boolean $disabled whether this page button is disabled
+     * @param boolean $active whether this page button is active
+     * @return string the rendering result
+     */
+    protected function renderPageButton($label, $page, $class, $disabled, $active)
+    {
+        $options = ['class' => $class === '' ? null : $class];
+        if ($active) {
+            Html::addCssClass($options, $this->activePageCssClass);
+        }
+        if ($disabled) {
+            Html::addCssClass($options, $this->disabledPageCssClass);
+
+            return Html::tag('li', Html::tag('span', $label), $options);
+        }
+        $linkOptions = $this->linkOptions;
+        $linkOptions['data-page'] = $page;
+
+        // turn off link for active item
+        if ($active && !$this->activeLinkable) {
+            return Html::tag('li', Html::tag('span', $label, $linkOptions), $options);
+        }
+
+        return Html::tag('li', Html::a($label, $this->pagination->createUrl($page), $linkOptions), $options);
     }
 }
